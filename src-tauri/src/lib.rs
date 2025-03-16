e13b28adab5a71932a13f4f841fb9d6c6b9078c9
emanuele-toma <toma.emanuele03@gmail.com>
2025-03-16 22:09:33 +0100

    :bug: Fix bug where window would not focus when trying to open another instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,12 +9,16 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_autostart::init(tauri_plugin_autostart::MacosLauncher::LaunchAgent, Some(vec![])))
+        .plugin(tauri_plugin_autostart::init(
+            tauri_plugin_autostart::MacosLauncher::LaunchAgent,
+            Some(vec![]),
+        ))
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
-            let _ = app
-                .get_webview_window("main")
-                .expect("no main window")
-                .set_focus();
+            let window = app.get_webview_window("main").expect("no main window");
+
+            let _ = window.show();
+            let _ = window.unminimize();
+            let _ = window.set_focus();
         }))
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_http::init())
